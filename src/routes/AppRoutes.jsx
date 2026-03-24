2d3b300a15032addb41b5924da06c9ce49756fff
Wadoo123 <zaiihadabi@gmail.com>
2026-03-24 09:51:40 +0700

    menambahkan logo di public, menambahkan halaman register dan pages nya, mengimport index.css di main.jsx, menginstall lucide-react dan react-router-dom

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,1 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import RegisterPage from '../pages/RegisterPage';
+
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/register" replace />} />
+
+        <Route path="/register" element={<RegisterPage />} />
+        
+        {/* <Route path="/login" element={<LoginPage />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRoutes;
